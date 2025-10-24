165ec2f65e1e566a460739abc1d248da387bf804
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-10-24 14:53:17 +0200

    `Fixed route function parameter in Express server routes`

--- Express Server/server.js
+++ Express Server/server.js
@@ -4,13 +4,13 @@ const app = express();
 const path = require("path")
 
 app.use(bodyParser.urlencoded({ extended: false}))
-app.get("/", (res) => {
+app.get("/", (req, res) => {
       res.sendFile(path.join(__dirname, "index.html"));
 });
-app.get("/about.html", (res) =>{
+app.get("/about.html", (req, res) =>{
       res.sendFile(path.join(__dirname, "about.html"))
 });
-app.get("/contact.html", (res) =>{
+app.get("/contact.html", (req, res) =>{
       res.sendFile(path.join(__dirname, "contact.html"))
 });
  const port = 3000;
