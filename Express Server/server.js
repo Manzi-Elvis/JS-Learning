090911dc4a9620ba458edcdbb127c7d09b263c8b
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-10-24 13:47:30 +0200

    Learning Node.js

---
+++ Express Server/server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express();
+const path = require("path")
+
+app.use(bodyParser.urlencoded({ extended: false}))
+app.get("/", (req, res) => {
+      res.sendFile(path.join(__dirname, "index.html"));
+});
+app.get("/about.html", (req, res) =>{
+      res.sendFile(path.join(__dirname, "about.html"))
+});
+app.get("/contact.html", (req, res) =>{
+      res.sendFile(path.join(__dirname, "contact.html"))
+});
+ const port = 3000;
+ app.listen(port, () => {
+      console.log(`Server running at port: ${port}`)
+ });
