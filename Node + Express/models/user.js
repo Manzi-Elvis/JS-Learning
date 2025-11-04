a51470637b1ce05dc8ab61289dc21ddc1164cd95
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-11-04 11:18:09 +0200

    using bycrypt to hash a password

--- Node + Express/models/user.js
+++ Node + Express/models/user.js
@@ -5,7 +5,11 @@ const userSchema = new mongoose.Schema({
       email: {type: String, required: true, unique: true, lowercase: true },
       password:{type: String, required: true, trim: true}
 });
-
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 const User = mongoose.model('User' , userSchema);
 
 module.exports = User
