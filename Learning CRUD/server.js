2907aea486383e62a49fa69c4c7bd82f589e9242
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-11-04 19:38:28 +0200

    ...

--- Learning CRUD/server.js
+++ Learning CRUD/server.js
@@ -1,56 +1,10 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const bcrypt = require('bcrypt');
-
+console.log('Mongo URI:', process.env.MONGO_URI);
 const app = require('./app');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
+
 const PORT = process.env.PORT || 5000;
+
 connectDB();
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
-// CREATE user
-app.post('/users', async (req, res) => {
-  try {
-    const { firstName, lastName, email, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    await User.create({ firstName, lastName, email, password: hashedPassword });
-    res.redirect('/');
-  } catch (err) {
-    console.error(err);
-    res.send('Error creating user');
-  }
-});
-
-// UPDATE user
-app.put('/users/:id', async (req, res) => {
-  try {
-    const { firstName, lastName, email, password } = req.body;
-    const user = await User.findById(req.params.id);
-
-    if (!user) return res.send('User not found');
-
-    user.firstName = firstName;
-    user.lastName = lastName;
-    user.email = email;
-
-    // Only hash new password if provided
-    if (password.trim() !== '') {
-      user.password = await bcrypt.hash(password, 10);
-    }
-
-    await user.save();
-    res.redirect('/');
-  } catch (err) {
-    console.error(err);
-    res.send('Error updating user');
-  }
-});
-// DELETE user
-app.delete('/users/:id', async (req, res) => {
-  try {
-    await User.findByIdAndDelete(req.params.id);
-      res.redirect('/');
-      } catch (err) {
-            console.error(err);
-            res.send('Error deleting user');
-      }
-});
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
