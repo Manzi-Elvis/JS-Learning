e286ba25f685c9f8d37b5ca55d937cea72e90994
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-11-04 20:30:19 +0200

    ...

--- Learning CRUD/app.js
+++ Learning CRUD/app.js
@@ -11,13 +11,8 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
 
-// Serve static HTML from views
 app.use(express.static(path.join(__dirname, 'views')));
-
-// API Routes
 app.use('/api/users', userRoutes);
-
-// Default route to serve HTML
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
