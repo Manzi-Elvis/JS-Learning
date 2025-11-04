279d0813376fc9acf289264a9243a520f2091618
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-11-04 14:36:27 +0200

    ...

--- Node + Express/controllers/userController.js
+++ Node + Express/controllers/userController.js
@@ -1,33 +1,39 @@
-const User = require('../models/user')
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
-//Create a new user
-exports.createUser = async(req, res) => {
-      try{
-            const { firstName, lastName, email, password } = req.body;
+exports.createUser = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
 
-            if(!firstName || !lastName || email || password ){
-                  return res.status(400).json({message: 'All fields are required'})
-            }
+    if (!firstName || !lastName || !email || !password) {
+      return res.status(400).json({ message: 'All fields are required' });
+    }
 
-            const existing = await User.findOne({email});
-            if(existing){
-                  return res.status(400).json({message: 'Email already exists'})
-            }
-            const user = await User.create({firstName, lastName, email, password});
-            res.status(201).json(user);
-      }
-      catch(error){
-            res.status(500).json({ message: error.message });
-      }
+    const existing = await User.findOne({ email });
+    if (existing) {
+      return res.status(400).json({ message: 'Email already exists' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
-//Get all users
 exports.getAllUsers = async (req, res) => {
-      try{
-            const users = await User.find();
-            res.json(users)
-      }
-      catch(error) {
-            res.status(500).json({message: error.message})
-      }
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
