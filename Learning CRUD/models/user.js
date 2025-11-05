3fc4381add214edb6c9abfe2faf2ea3cd72c4d29
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-11-05 09:18:43 +0200

    ...

--- Learning CRUD/models/user.js
+++ Learning CRUD/models/user.js
@@ -1,10 +1,32 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  firstName: { 
+    type: String, 
+    required: [true, 'First name is required'] 
+  },
+  lastName: { 
+    type: String, 
+    required: [true, 'Last name is required'] 
+  },
+  email: { 
+    type: String, 
+    required: [true, 'Email is required'], 
+    unique: true,
+    match: [/\S+@\S+\.\S+/, 'Please use a valid email address']
+  },
+  password: { 
+    type: String, 
+    required: [true, 'Password is required'],
+    minlength: [8, 'Password must be at least 8 characters long'],
+    validate: {
+      validator: function(value) {
+        // Strong password regex:
+        return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&._-])[A-Za-z\d@$!%*?&._-]{8,}$/.test(value);
+      },
+      message: 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.'
+    }
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
